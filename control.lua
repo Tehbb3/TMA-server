2af6ea862d2fefce519ee01f7c28eaf86241721f
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2022-04-06 08:53:07 -0700

    Update control.lua

--- control.lua
+++ control.lua
@@ -6,7 +6,7 @@ local config = {
         modem = "back"
     },
     network = {
-        serverPort = 1000, -- port to send to server
+        serverPort = 2000, -- port to send to server
         clientPort = 2000, -- listen port for client
     }
 
@@ -34,6 +34,24 @@ local function display(dir)
 end
 
 
+function split(pString, pPattern)
+    local Table = {}  -- NOTE: use {n = 0} in Lua-5.0
+    local fpat = "(.-)" .. pPattern
+    local last_end = 1
+    local s, e, cap = pString:find(fpat, 1)
+    while s do
+           if s ~= 1 or cap ~= "" then
+          table.insert(Table,cap)
+           end
+           last_end = e+1
+           s, e, cap = pString:find(fpat, last_end)
+    end
+    if last_end <= #pString then
+           cap = pString:sub(last_end)
+           table.insert(Table, cap)
+    end
+    return Table
+ end
 
 local function listen()
 
