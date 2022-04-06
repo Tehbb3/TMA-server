9b973191b8319cac66e3a654b97ef5e3ecfcfca9
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2022-04-06 09:18:39 -0700

    Update slave.lua

--- slave.lua
+++ slave.lua
@@ -60,7 +60,7 @@ local function listen()
         -- print("Message contents: \n"..message)
         -- print("Sender is "..(senderDistance or "an unknown number of").." blocks away")
         term.setCursorPos(1, 19)
-        term.write("S>"..message)
+        term.write("C>"..message.com)
         currentAction.type = message.com
         currentAction.times = message.times
 
