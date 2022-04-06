0651d79a118f29f17e2b91f850e4804f7aa1ac0a
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2022-04-06 04:05:53 -0700

    Initial commit

---
+++ server.lua
@@ -0,0 +1,24 @@
+
+-- configuration
+local displaySide = 'left'
+
+function display(dir)
+   local monitor = peripheral.wrap("back") 
+   monitor.setTextScale(2)
+
+   monitor.setCursorPos(1,1)
+   monitor.clearLine()
+   monitor.write("Display")
+end
+
+
+
+
+local runHandeler = true -- value so main loop can be killed
+while runHandeler do -- main loop
+
+    display(displaySide)
+
+    os.sleep(0.5) -- just to limit main loop
+
+end
