2ad461df0efe4be7f961b55330197257d716874f
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2022-04-06 05:20:25 -0700

    Update control.lua

--- control.lua
+++ control.lua
@@ -22,7 +22,7 @@ print("/_/ /_/  /_/_/ |_|  /___/ ")
 -- local monitor = peripheral.wrap(config.side.monitor) 
 local modem = peripheral.wrap(config.side.modem)
 
-function display(dir)
+local function display(dir)
    
    monitor.setTextScale(2)
 
@@ -31,33 +31,58 @@ function display(dir)
    monitor.write("Display")
 end
 
+
+local function listen()
+
+    local runModule = true -- value so main loop can be killed
+    while runModule do -- main loop
+
+
+        local event, modemSide, senderChannel, 
+        replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    
+        print("===== Message Recive ======")
+        print("Channel: "..senderChannel)
+        print("Reply channel: "..replyChannel)
+        print("Modem on "..modemSide.." side")
+        print("Message contents: \n"..message)
+        print("Sender is "..(senderDistance or "an unknown number of").." blocks away")
+
+    end
+
+end
+
+
+local function ui()
+
+    local runModule = true -- value so main loop can be killed
+    while runModule do -- main loop
+
+
+        local input = read("*")
+        print(input)
+    
+    end
+
+end
+
+
+
+
 print("Display set to monitor: "..config.side.monitor.."\n")
 
 modem.open(config.network.clientPort)
 print("Opended server port: "..config.network.serverPort)
 
 
-print("loading main loop.")
-local runHandeler = true -- value so main loop can be killed
-while runHandeler do -- main loop
+print("main function.")
+
+parallel.waitForAny(
+    listen,
+    ui
+)
+
 
     -- display(displaySide)
     -- os.sleep(0.5) -- just to limit main loop
 
-    -- local event, modemSide, senderChannel, 
-    -- replyChannel, message, senderDistance = os.pullEvent("modem_message")
-  
-    -- print("===== Message Recive ======")
-    -- print("Channel: "..senderChannel)
-    -- print("Reply channel: "..replyChannel)
-    -- print("Modem on "..modemSide.." side")
-    -- print("Message contents: \n"..message)
-    -- print("Sender is "..(senderDistance or "an unknown number of").." blocks away")
-
-    local input = read("*")
-    print(input)
-
-end
-
-
-print("After loop???")
