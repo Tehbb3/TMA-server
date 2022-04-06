cf9cd9e5fdd4c166f933091d72b69058d8077c0e
Tehbb <62781302+Tehbb3@users.noreply.github.com>
2022-04-06 08:26:47 -0700

    Update slave.lua

--- slave.lua
+++ slave.lua
@@ -8,11 +8,13 @@ local config = {
     network = {
         serverPort = 1000, -- port to send to server
         clientPort = 2000, -- listen port for client
+        slavePort = 3000, -- listen port for the slave
     }
 
 }
 
-
+local autoRefuel = true
+local autoRefuelThreshold = 200
 
 print("________  ______      ____")
 print("/_  __/  |/  / _ |____/ __/")
@@ -24,6 +26,7 @@ local currentAction = {
     times = 0,
 }
 local slaveFuel = 0
+
 -- local monitor = peripheral.wrap(config.side.monitor) 
 local modem = peripheral.wrap(config.side.modem)
 
@@ -64,6 +67,12 @@ local function listen()
     end
 
 end
+local function rprint(text)
+
+    modem.transmit(config.network.serverPort, config.network.slavePort, "#"..text)
+
+    print(text)
+end
 
 
 
@@ -95,51 +104,112 @@ local function action()
     local runModule = true -- value so main loop can be killed
     while runModule do -- main loop
 
+        
+        if autoRefuel == true then
+            if turtle.getFuelLevel() < autoRefuelThreshold then
+                turtle.refuel(1)
+            else
+                rprint("Fuel level : "..turtle.getFuelLevel())
+            end
+        else
+
+
+
+
         if (currentAction.type == "NO") or (currentAction.times == 0) then
-            print("NO ACTION - "..slaveFuel)
+            rprint("NO ACTION - "..slaveFuel)
             os.sleep(0.5)
         else
 
-            -- if currentAction.type == "MF" then
-            --     turtle.foward()
+
+            -- MOVEMENT
+            if currentAction.type == "MF" then
+                turtle.forward()
+                rprint("Move forward")
+            end
+            if currentAction.type == "MB" then
+                turtle.back()
+                rprint("Move back")
+            end
+            if currentAction.type == "MU" then
+                turtle.up()
+                rprint("Move up")
+            end
+            if currentAction.type == "MD" then
+                turtle.down()
+                rprint("Move down")
+            end
+            if currentAction.type == "TL" then
+                turtle.turnLeft()
+                rprint("Turn left")
+            end
+            if currentAction.type == "TR" then
+                turtle.turnRight()
+                rprint("Turn right")
+            end
+            
+            -- ACTION
+            if currentAction.type == "DF" then
+                turtle.dig()
+                rprint("Dig forward")
+            end
+            if currentAction.type == "DU" then
+                turtle.digUp()
+                rprint("Dig up")
+            end
+            if currentAction.type == "DD" then
+                turtle.digDown()
+                rprint("Dig down")
+            end
+
+
+            -- tunnel
+            if currentAction.type == "D1" then
+                turtle.dig()
+                turtle.forward()
+                rprint("Tunnel 1")
+            end
+
+
+            -- if currentAction.type == "" then
+            --     turtle.
+            --     rprint("")
             -- end
 
 
-            local slaveCommands = {
-                {id="NO", fuelUse=false, action="print(\"Nop\")"},
-                {id="RF", fuelUse=true, action="turtle.refuel(1) slaveFuel = slaveFuel + 80 print('Refueling 1')"},
-                {id="MF", fuelUse=true, action="turtle.forward() print('forward')"},
-                {id="MB", fuelUse=true, action="turtle.back()    print('back')"},
-                {id="MU", fuelUse=true, action="turtle.up()      print('up')"},
-                {id="MD", fuelUse=true, action="turtle.down()    print('down')"},
-                {id="TL", fuelUse=true, action="turtle.turnLeft()print('turn left')"},
-                {id="TR", fuelUse=true, action="turtle.turnRight()print('turn right')"},
-                {id="DF", fuelUse=true, action="turtle.dig() print('dig')"},
-                {id="DU", fuelUse=true, action=""},
-                {id="DD", fuelUse=true, action=""},
-                {id="DIE", fuelUse=true, action="exit()"},
-                {id="NAH", fuelUse=true, action="print('just the noo')"},
-            }
 
+            -- local slaveCommands = {
+            --     {id="NO", fuelUse=false, action="print(\"Nop\")"},
+            --     {id="RF", fuelUse=true, action="turtle.refuel(1) slaveFuel = slaveFuel + 80 print('Refueling 1')"},
+            --     {id="MF", fuelUse=true, action="turtle.forward() print('forward')"},
+            --     {id="MB", fuelUse=true, action="turtle.back()    print('back')"},
+            --     {id="MU", fuelUse=true, action="turtle.up()      print('up')"},
+            --     {id="MD", fuelUse=true, action="turtle.down()    print('down')"},
+            --     {id="TL", fuelUse=true, action="turtle.turnLeft()print('turn left')"},
+            --     {id="TR", fuelUse=true, action="turtle.turnRight()print('turn right')"},
+            --     {id="DF", fuelUse=true, action="turtle.dig() print('dig')"},
+            --     {id="DU", fuelUse=true, action=""},
+            --     {id="DD", fuelUse=true, action=""},
+            --     {id="DIE", fuelUse=true, action="exit()"},
+            --     {id="NAH", fuelUse=true, action="print('just the noo')"},
+            -- }
+            -- n = 1
+            -- while slaveCommands[n] ~= nil do
 
-
-            n = 1
-            while slaveCommands[n] ~= nil do
-
-                if slaveCommands[n].id == currentAction.type then
-                    local func, err = loadstring(slaveCommands[n].action)
+            --     if slaveCommands[n].id == currentAction.type then
+            --         local func, err = loadstring(slaveCommands[n].action)
                     
-                    if func then -- check if the function is loaded
-                        func() -- run the function
-                        if slaveCommands[n].fuelUse == true then
-                            slaveFuel = slaveFuel - 1
-                        end
-                    else
-                        print("Error: ", err) -- error loading the string
-                    end
-                end
-                n = n + 1
-            end
+            --         if func then -- check if the function is loaded
+            --             func() -- run the function
+            --             if slaveCommands[n].fuelUse == true then
+            --                 slaveFuel = slaveFuel - 1
+            --             end
+            --         else
+            --             print("Error: ", err) -- error loading the string
+            --         end
+            --     end
+            --     n = n + 1
+            -- end
 
             currentAction.times = currentAction.times - 1 -- decrement action
 
